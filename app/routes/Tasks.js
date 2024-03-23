12011a06b422e3277ef65af7d19287189576468d
Arthur Araujo <arthurguiaraujo123@gmail.com>
2024-03-23 10:34:02 -0300

    Taks.js

--- app/routes/Tasks.js
+++ app/routes/Tasks.js
@@ -22,7 +22,7 @@ router.post('/', async (req, res) => {
     res.status(201).json(newTask);
   }
   catch (err){
-    res.status(400).json({message: err.message});
+    res.status(500).json({message: err.message});
   }
 });
 
